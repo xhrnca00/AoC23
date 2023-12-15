a0374888d32fc923c0935be499b0f68d14885536
hrnec <34456624+hrnec@users.noreply.github.com>
2023-12-15 16:49:13 +0100

    start 13

---
+++ src/bin/13.rs
@@ -0,0 +1,35 @@
+use anyhow::Result;
+
+const DAY: u8 = 13;
+
+fn part_1(input: &str) -> Result<u32> {
+    Ok(input.trim().parse()?)
+}
+
+fn part_2(input: &str) -> Result<u32> {
+    todo!("Implement part 2. Input len: {}", input.len())
+}
+
+fn main() -> Result<()> {
+    aoc::solve_all(DAY, part_1, part_2)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() -> Result<()> {
+        let res = "405";
+        aoc::assert_output_matches_str(DAY, "example1", part_1, res)?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_2() -> Result<()> {
+        let res = "2";
+        aoc::assert_output_matches_str(DAY, "example2", part_2, res)?;
+        Ok(())
+    }
+}
