0b04b480111fe8ccf396647a08fc1dc5c682253e
xhrnca00 <89995247+xhrnca00@users.noreply.github.com>
2023-12-17 00:01:57 +0100

    don't time any I/O; remove saving solution output

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
-use std::{
-    fmt::Display,
-    fs::File,
-    io::{BufWriter, Read, Write},
-    path::PathBuf,
-};
+use std::{fmt::Display, fs::File, io::Read, path::PathBuf};
 
 use anyhow::Result;
 use project_root::get_project_root;
@@ -13,20 +8,16 @@ pub fn solve_all<T1: Display, T2: Display>(
     part_1: fn(&str) -> Result<T1>,
     part_2: fn(&str) -> Result<T2>,
 ) -> Result<()> {
-    let output_file = File::create(get_output_path(task_num, None)?)?;
-    let mut writer = BufWriter::new(output_file);
     let mut input_file = File::open(get_input_path(task_num, None)?)?;
     let mut input = String::new();
     input_file.read_to_string(&mut input)?;
     let start = std::time::Instant::now();
     let res1 = part_1(&input)?;
-    write!(writer, "{res1}\n")?;
-    eprintln!("Part 1: {res1}");
     let res2 = part_2(&input)?;
-    write!(writer, "{res2}\n")?;
+    let elapsed = start.elapsed();
+    eprintln!("Part 1: {res1}");
     eprintln!("Part 2: {res2}");
-    eprintln!("Finished in {:?}!", start.elapsed());
-    writer.flush()?;
+    eprintln!("Finished in {:?}!", elapsed);
     Ok(())
 }
 
@@ -39,19 +30,6 @@ pub fn get_input_path(task_num: u8, suffix: Option<&str>) -> Result<PathBuf> {
         filename.push_str(suffix);
     }
     filename.push_str(".in");
-    path.push(filename);
-    Ok(path)
-}
-
-pub fn get_output_path(task_num: u8, suffix: Option<&str>) -> Result<PathBuf> {
-    let mut path = get_project_root()?;
-    path.push("data");
-    let mut filename = format!("{:02}", task_num);
-    if let Some(suffix) = suffix {
-        filename.push('_');
-        filename.push_str(suffix);
-    }
-    filename.push_str(".out");
     path.push(filename);
     Ok(path)
 }
